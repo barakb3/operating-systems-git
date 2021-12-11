d33e9d1c4b36b0416744dcf59bef16ebb8ec3ae9
barak <brkbsn@gmail.com>
2021-12-11 12:54:41 +0200

    Add rec05, rec 06 and the driver skeleton, that is, the sekeleton of the Makefile and the message_slot.c

---
+++ hw03/message_slot.c
@@ -0,0 +1,35 @@
+/* We are part of the kernel */
+#undef __KERNEL__
+#define __KERNEL__
+
+
+#undef MODULE
+#define MODULE
+
+/* included for all kernel modules */
+#include <linux/module.h>
+/* included for __init and __exit macros */
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+
+/* loader */
+static int __init msgslot_init(void)
+{
+    /* when soemthing fails returns non-zero value */
+    if (0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+/* unloader */
+static void __exit msgslot_exit(void)
+{
+
+}
+
+/* Defining initialization and exit functions of the driver */
+module_init(msgslot_init);
+module_exit(msgslot_exit);
