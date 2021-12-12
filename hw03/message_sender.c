0a6aab608bec2aaafe27c5b877388883cd0d39ce
barakb3 <brkbsn@gmail.com>
2021-12-12 17:19:45 +0200

    First try

---
+++ hw03/message_sender.c
@@ -0,0 +1,49 @@
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "message_slot.h"
+
+void main(int argc, char **argv)
+{
+    int fd;
+    char *endptr;
+    unsigned int channel_number = strtol(argv[2], &endptr, 10);
+    if (argc != 4)
+    {
+        /* invalid number of arguments */
+        errno = EINVAL;
+        fprintf(stderr, "Invalid number of arguments.\nerrno: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    if ((fd = open(argv[1], O_WRONLY)) < 0)
+    {
+        /* open failed */
+        fprintf(stderr, "Open file failed.\nerrno: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    if (ioctl(fd, MSG_SLOT_CHANNEL, channel_number) < 0)
+    {
+        /* set channel failed */
+        fprintf(stderr, "Set channel failed.\nerrno: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(fd, argv[3], strlen(argv[3])) < 0)
+    {
+        /* writing failed */
+        fprintf(stderr, "Writing failed.\nerrno: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(fd) < 0)
+    {
+        /* close failed */
+        fprintf(stderr, "Close failed.\nerrno: %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
--- hw03/message_slot.c
+++ hw03/message_slot.c
@@ -2,69 +2,169 @@
 #undef __KERNEL__
 #define __KERNEL__
 
-
 #undef MODULE
 #define MODULE
 
-/* included for all kernel modules */
-#include <linux/module.h>
-/* included for __init and __exit macros */
-#include <linux/init.h>
-/* included for register_chrdev */
-#include <linux/fs.h>
+#include <linux/kernel.h>  /* We're doing kernel work */
+#include <linux/module.h>  /* Specifically, a module */
+#include <linux/fs.h>      /* for register_chrdev */
+#include <linux/uaccess.h> /* for get_user and put_user */
+#include <linux/slab.h>    /* included for kmalloc flag GFP_KERNEL */
 
-#define SUCCESS 0
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include "message_slot.h"
 
 MODULE_LICENSE("GPL");
 
+/* Linked list structure for each device for different channels */
+/* each node represents a channel (id, channel itself, length of the message and a pointer to the next channel in the device) */
+typedef struct NODE
+{
+    unsigned int channel_id;
+    char channel[BUF_LEN];
+    unsigned int msg_len;
+    struct NODE *next;
+} NODE;
+
+/* a static array (initialized with zeros) for the different devices (minor numbers between 0 to 255) */
+typedef struct device
+{
+    int initialized;
+    NODE *head;
+} device;
+
+static device devices[256];
+
 /* device functions */
-static int device_open(struct inode* inode, struct file* file)
+static int device_open(struct inode *inode, struct file *file)
 {
-    return SUCCESS;
-}
-
-static int device_release(struct inode* inode, struct file* file) 
-{
-    return SUCCESS;
-}
-
-static ssize_t device_read(struct file* file, char __user* buffer, size_t length, loft_t offset)
-{
-
-}
-
-static ssize_t device_write(struct file* file, const char __user* buffer, size_t length, loft_t offset)
-{
-    return 
-}
-
-static long device_ioctl(struct file* file, unsigned int ioctl_command_id, unsigned int ioctl_param)
-{
-    if (ioctl_command_id == MSG_SLOT_CHANNEL)
+    unsigned int minor = iminor(inode);
+    if (devices[minor].initialized == 0)
     {
-
+        /* new device */
+        devices[minor].initialized = 1;
+        devices[minor].head = NULL;
     }
-    else
+
+    return SUCCESS;
+}
+
+static int device_release(struct inode *inode, struct file *file)
+{
+    return SUCCESS;
+}
+
+static ssize_t device_read(struct file *file, char __user *buffer, size_t length, loft_t offset)
+{
+    NODE *node = (NODE *)file->private_data;
+    if (node == NULL)
     {
+        /* no channel has been set on the file descriptor */
         errno = EINVAL;
         return -1;
     }
+
+    if (node->msg_len == 0)
+    {
+        /* no message exists on the channel */
+        errno = EWOULDBLOCK;
+        return -1;
+    }
+
+    if (length < node->msg_len)
+    {
+        /* provided buffer is too small */
+        errno = ENOSPC;
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i < node->msg_len && i < BUF_LEN; i++)
+    {
+        put_user(node->channel[i], &buffer[i]);
+    }
+    return i;
 }
-/* device setup */
-struct file_operations Fops =
+
+static ssize_t device_write(struct file *file, const char __user *buffer, size_t length, loft_t offset)
 {
-  .owner    = THIS_MODULE, // Required for correct count of module usage. This prevents the module from being removed while used.
-  .read     = device_read,
-  .write    = device_write,
-  .open     = device_open,
-  .release  = device_release,
+    NODE *node = (NODE *)file->private_data;
+    if (node == NULL)
+    {
+        /* no channel has been set on the file descriptor */
+        errno = EINVAL;
+        return -1;
+    }
+
+    if (length == 0 || length > 128)
+    {
+        /* message size error */
+        errno = EMSGSIZE;
+        return -1;
+    }
+
+    int i;
+    for (i = 0; i < length && i < BUF_LEN; i++)
+    {
+        get_user(node->channel[i], &buffer[i]);
+    }
+    node->msg_len = i;
+    return i;
+}
+
+static long device_ioctl(struct file *file, unsigned int ioctl_command_id, unsigned int ioctl_param)
+{
+    if (ioctl_command_id != MSG_SLOT_CHANNEL || ioctl_param == 0)
+    {
+        /* invalid arguments when calling ioctl() */
+        errno = EINVAL;
+        return -1;
+    }
+
+    inode *inode = file->d_entry->d_inode;
+    unsigned int minor = iminor(inode);
+    NODE *last = devices[minor].head;
+    unsigned int channel_id = 0;
+
+    while (ioctl_param != channel_id)
+    {
+        if (last == NULL)
+        {
+            last = (NODE *)kmalloc(sizeof(NODE), GFP_KERNEL);
+            if (last == NULL)
+            {
+                /* failed allocating memory */
+                errno = ENOMEM;
+                return -1;
+            }
+            last->channel_id = ioctl_param;
+            last->next = NULL;
+            last->msg_len = 0;
+            break;
+        }
+        last = last->next;
+    }
+    file->private_data = (void *)last;
+    return 0;
+}
+
+/* device setup */
+struct file_operations fops =
+    {
+        .owner = THIS_MODULE, // Required for correct count of module usage. This prevents the module from being removed while used.
+        .read = device_read,
+        .write = device_write,
+        .open = device_open,
+        .release = device_release,
 };
 
 /* loader */
 static int __init msgslot_init(void)
 {
     /* when soemthing fails returns non-zero value */
-    int ret_register_chrdev = register_chrdev(240, "msgslot", &Fops);
+    int ret_register_chrdev = register_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME, &fops);
     if (ret_register_chrdev < 0)
     {
         printk(KERN_ERR "msgslot device registration failed for %d", ret_register_chrdev);
@@ -76,9 +176,22 @@ static int __init msgslot_init(void)
 /* unloader */
 static void __exit msgslot_exit(void)
 {
-    unregister_chrdev(240, "msgslot");
+    int i;
+    NODE *curr, *last;
+    for (i = 0; i < 256, i++)
+    {
+        curr = NULL;
+        last = devices[i].head;
+        while (last != NULL)
+        {
+            curr = last;
+            last = curr->next;
+            kfree(curr);
+        }
+    }
+    unregister_chrdev(MAJOR_NUM, DEVICE_RANGE_NAME);
 }
 
-/* Defining initialization and exit functions of the driver */
+/* defining initialization and exit functions of the driver */
 module_init(msgslot_init);
 module_exit(msgslot_exit);
