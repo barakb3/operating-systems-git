24839e815b896bca4989a96d4131949fc4d02bda
barakb3 <brkbsn@gmail.com>
2021-12-29 19:45:27 +0200

    some order with returns

--- hw04/pfind1.c
+++ hw04/pfind1.c
@@ -33,7 +33,7 @@ DIR_FIFO_Q *initialize_directory_queue(int *status)
     if (dir_q == NULL)
     {
         *status = FAILURE;
-        fprintf(stderr, "Failed allocating memory for the directory queue due to errno: %s", strerror(errno));
+        fprintf(stderr, "Failed allocating memory for the directory queue due to errno: %s\n", strerror(errno));
         return NULL;
     }
 
@@ -41,7 +41,7 @@ DIR_FIFO_Q *initialize_directory_queue(int *status)
     if (dir_q->first == NULL)
     {
         *status = FAILURE;
-        fprintf(stderr, "Failed allocating memory for the directory entry due to errno: %s", strerror(errno));
+        fprintf(stderr, "Failed allocating memory for the directory entry due to errno: %s\n", strerror(errno));
         return NULL;
     }
     dir_q->first->dir = NULL;
@@ -70,13 +70,15 @@ int iterate_dir(DIR_FIFO_Q *dir_q, char *path, const char *search_term)
             continue;
         }
 
-        /* extracting dirent type by modifing the stat structure to represent the current dirent */
+        /* modifying the path of the current dirent */
         strcpy(curr_path, path);
         strcat(curr_path, curr_name);
+
+        /* extracting dirent type by modifing the stat structure to represent the current dirent */
         if (stat(curr_path, &curr_statbuf) != 0)
         {
-            fprintf(stderr, "Failed when tried to check dir status due to errno: %s", strerror(errno));
-            return 1;
+            fprintf(stderr, "Failed when tried to check dir status due to errno: %s\n", strerror(errno));
+            return FAILURE;
         }
 
         if (S_ISDIR(curr_statbuf.st_mode))
@@ -87,7 +89,15 @@ int iterate_dir(DIR_FIFO_Q *dir_q, char *path, const char *search_term)
                 /* directory can't be searched */
                 printf("Directory %s: Permission denied.\n", curr_path);
             }
-            enqueue(dir_q, new_dir);
+            else
+            {
+                /* directory can be searched */
+                if (enqueue(dir_q, new_dir) == FAILURE)
+                {
+                    /* enqueuing failed */
+                    return FAILURE;
+                }
+            }
         }
         else
         {
@@ -101,7 +111,7 @@ int iterate_dir(DIR_FIFO_Q *dir_q, char *path, const char *search_term)
     }
     if (errno != 0)
     {
-        fprintf(stderr, "Failed when tried to read dir content due to errno: %s", strerror(errno));
+        fprintf(stderr, "Failed when tried to read dir ( readdir() ) content in some iteration due to errno: %s\n", strerror(errno));
         return FAILURE;
     }
     return SUCCESS;
@@ -121,7 +131,7 @@ int enqueue(DIR_FIFO_Q *dir_q, DIR *dir)
     if (dir_q->last->next == NULL)
     {
         status = FAILURE;
-        fprintf(stderr, "Failed allocating memory for the directory entry due to errno: %s", strerror(errno));
+        fprintf(stderr, "Failed allocating memory for the directory entry due to errno: %s\n", strerror(errno));
     }
     dir_q->last = dir_q->last->next;
     dir_q->last->dir = dir;
@@ -132,45 +142,52 @@ int enqueue(DIR_FIFO_Q *dir_q, DIR *dir)
 int main(int argc, char *argv[])
 {
     int status = SUCCESS;
-    char *path;
-    const char *search_term;
+    char *path, *search_term;
+    DIR *root;
+    DIR_FIFO_Q *dir_q;
 
-    /* initializing the directory queue */
-    DIR_FIFO_Q *dir_q = initialize_directory_queue(&status);
-    if (dir_q == NULL)
-    {
-        /* initializing failed */
-        return status;
-    }
-    
     if (argc != 4)
     {
         /* numbar of arguments isn't valid */
         status = FAILURE;
-        fprintf(stderr, "Number of arguments isn't valid");
+        fprintf(stderr, "Number of arguments isn't valid\n");
         return status;
     }
 
     path = argv[1];
     search_term = argv[2];
+    root = opendir(path);
 
-    if ((dir_q->first->dir = opendir(argv[2])) == NULL)
+    if (root == NULL)
     {
         /* root directory can't be searched */
         status = FAILURE;
-        fprintf(stderr, "Root directory can't be searched, failed due to errno: %s", strerror(errno));
+        fprintf(stderr, "Root directory can't be searched, failed due to errno: %s\n", strerror(errno));
         return status;
     }
 
-    /* create argv[3] threads */
+    /* initializing the directory queue */
+    dir_q = initialize_directory_queue(&status);
+    if (dir_q == NULL)
+    {
+        /* initializing failed */
+        return status;
+    }
+
+    dir_q->first->dir = root;
+    dir_q->first->next = NULL;
+
+    /****** create argv[3] threads ******/
 
     while (dir_q->first != NULL)
     {
-        if ((status = iterate_dir(dir_q, path, search_term)) == 1)
+        status = iterate_dir(dir_q, path, search_term);
+        if (status == FAILURE)
         {
             /* Failed when tried to read dir content */
             return status;
         }
         dequeue(dir_q);
+        /* free the dequeued entry????????? */
     }
 }
