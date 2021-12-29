78120b8e1d282a5bd39ee6242bdf7d668015b397
barakb3 <brkbsn@gmail.com>
2021-12-29 18:47:36 +0200

    compilation fixes

--- hw04/pfind.c
+++ hw04/pfind.c
@@ -79,7 +79,7 @@ int iterate_dir(DIR_FIFO_Q *dir_q, char *path, const char *search_term)
             return 1;
         }
 
-        if (curr_stat->st_mode & S_IFMT == SIFDIR)
+        if (SIFDIR(curr_stat->st_mode))
         {
             /* dirent is a directory */
             if ((new_dir = opendir(curr_path)) == NULL)
