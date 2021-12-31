2f021638313b7308e4af7423db38e345fdfe4522
barakb3 <brkbsn@gmail.com>
2021-12-31 20:45:00 +0200

    Debugging with prints

--- hw04/pfind1.c
+++ hw04/pfind1.c
@@ -358,7 +358,7 @@ THREAD_ENTRY *dequeue_thread(THREAD_FIFO_Q *thread_q)
     THREAD_ENTRY *ret = thread_q->first;
     thread_q->first = thread_q->first->next;
     thread_q->len--;
-    printf("length of thread_q after dequeuing thread is %d", thread_q->len);
+    printf("length of thread_q after dequeuing thread is %d\n", thread_q->len);
     return ret;
 }
 
