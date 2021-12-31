190103fd04aa50007ff87f21b2bc4a3a2d243e20
barakb3 <brkbsn@gmail.com>
2021-12-31 18:03:05 +0200

    Debugging with prints

--- hw04/pfind1.c
+++ hw04/pfind1.c
@@ -265,7 +265,7 @@ void scan_dir(THREAD_ENTRY *my_thread_entry)
                 if (next_thread_in_queue != NULL)
                 {
                     next_thread_in_queue->dir = new_dir;
-                    next_thread_in_queue->path = curr_path;
+                    strcpy(next_thread_in_queue->path, curr_path);
                     printf("path entered: %s\n", next_thread_in_queue->path);
                     pthread_cond_signal(next_thread_in_queue->my_condition_variable);
                 }
