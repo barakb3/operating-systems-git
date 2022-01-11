0b54542551e86f786225cbe4ef79831698518c6c
barakb3 <brkbsn@gmail.com>
2022-01-11 17:25:51 +0200

    debugging

--- hw04/pfind.c
+++ hw04/pfind.c
@@ -61,7 +61,7 @@ DIR_FIFO_Q *dir_q;
 THREAD_FIFO_Q *thread_q;
 
 char *search_term;
-char root_path[PATH_MAX];
+char *root_path;
 
 pthread_mutex_t thread_initializer;
 pthread_mutex_t queues_access;
@@ -411,7 +411,10 @@ int main(int argc, char *argv[])
         exit(status);
     }
 
+    /*
     strcpy(root_path, argv[1]);
+    */
+    root_path = argv[1];
     search_term = argv[2];
     num_of_threads = atoi(argv[3]);
     root = opendir(root_path);
